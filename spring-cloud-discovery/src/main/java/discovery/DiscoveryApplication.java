5405780e5eb48b979647342de42bb3c474f9afd9
dolphintwo <dtdinghui@126.com>
2018-08-10 16:03:30 +0800

    [ADD] add discovery eureka

---
+++ spring-cloud-discovery/src/main/java/discovery/DiscoveryApplication.java
@@ -0,0 +1,18 @@
+package discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author DolphinTwo
+ * @date 2018/8/10
+ */
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryApplication.class, args);
+    }
+}
