71e686fe7174dcfb2ab65ec60637c262cfefbddd
dolphintwo <dtdinghui@126.com>
2018-08-11 12:49:58 +0800

    [MOD] modify

--- spring-cloud-config-client/src/main/java/config_client/ConfigClientApplication.java
+++ spring-cloud-config-client/src/main/java/config_client/ConfigClientApplication.java
@@ -2,11 +2,9 @@ package config_client;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,8 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RefreshScope
 @RestController
-@Configuration
-@EnableAutoConfiguration
 public class ConfigClientApplication {
 
     @Value("${from}")
